d5d8a655e988355c727c230f216bb07e6bbae155
Michell <michellpine@hotmail.com>
2018-02-15 07:02:15 -0500

    router link

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lugares',
-  templateUrl: './lugares.component.html'
+  templateUrl: './lugares.component.html',
+  styleUrls: ['./lugares.component.css']
 })
 export class LugaresComponent {
   title = 'PlatziSquare';
